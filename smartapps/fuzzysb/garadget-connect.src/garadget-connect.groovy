25558ce0df5f99b5a95454dc7088368c76f9cd31
Stuart Buchanan <stuartb@broadbandtap.co.uk>
2017-12-19 03:05:43 +0000

    Updated 1.5 header text

--- smartapps/fuzzysb/garadget-connect.src/garadget-connect.groovy
+++ smartapps/fuzzysb/garadget-connect.src/garadget-connect.groovy
@@ -12,6 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * 12/12/2017 V1.5 fixed bug introduced in v1.4 on initialize function.
  * 12/12/2017 V1.4 debug logging changes. - btrenbeath
  * 21/04/2017 V1.3 added url encoding to username and password for when special characters are used, with thanks to pastygangster
  * 20/03/2017 V1.2 updated to refresh the garadget devices every 1 minute which is the minimum schedule allowed in ST
